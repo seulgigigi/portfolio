cfee2969f3b53d2b4d45a0a82ba36448bb4da5f4
seulgigigi <calambroalvin9@gmail.com>
2025-08-21 21:50:18 +0800

    assets

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -7,21 +7,21 @@ export default function Catalog() {
     {
       title: "",
       description: "A Pomodoro timer website with a built in to-do list and lofi playlist.",
-      image: "images/OIG.jpeg",
+      image: "/images/OIG.jpeg",
       link: "https://seulgigigi.github.io/pomi/",
       category: "HTML, CSS, Javascript"
     },
     {
       title: "",
       description: "A Filipino language learning app, gamified to cater to students.",
-      image: "images/filaro512.png",
+      image: "/images/filaro512.png",
       link: "https://seulgigigi.github.io/FiLaro/",
       category: "HTML, CSS, Javascript"
     },
     {
       title: "",
       description: "An inventory system prototype built in Excel for a small silog business.",
-      image: "images/invent.jpg",
+      image: "/images/invent.jpg",
       link: "https://docs.google.com/document/d/1_Wc_NlU_itlAlvMNNS28KFwF9AU-gjza/edit?usp=sharing&ouid=105250311011582006818&rtpof=true&sd=true",
       category: "Visual Basic"
     }
