c2bebe3b3158a71bfeb8830c2b6acda3646836ce
seulgigigi <calambroalvin9@gmail.com>
2025-08-21 22:06:54 +0800

    tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 //main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '/src/index.css'
+import App from '/src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
